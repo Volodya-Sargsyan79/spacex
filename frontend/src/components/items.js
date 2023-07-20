9a2fdf78b817d0095c5e0e84bcad4241385b0cf7
Volodya Sargsyan <volsarg@gmail.com>
2023-07-20 22:44:02 +0400

    items style

--- frontend/src/components/items.js
+++ frontend/src/components/items.js
@@ -1,9 +1,9 @@
 import React from "react";
 import '../style/Items.scss'
 
-export default function Items() {
+export default function Items({classes}) {
     return (
-        <div className="items_container">
+        <div className={`items_container ${classes}`}>
             <p>mi</p>
             <h1>250</h1>
             <p>nash dvor</p>
--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,18 +1,19 @@
 import React from "react";
 import Items from "../components/items";
+import '../style/Home.scss'
 
 export default function Home() {
     return (
-        <div>
+        <div className="home_container">
             <div></div>
-            <div>
-                <div>
-                    <Items />
-                    <Items />
+            <div className="home_container_items">
+                <div className="home_container_items--first">
+                    <Items classes="top_left" />
+                    <Items classes="top_right"/>
                 </div>
-                <div>
-                    <Items />
-                    <Items />
+                <div className="home_container_items--second">
+                    <Items classes=""/>
+                    <Items classes=""/>
                 </div>
             </div>
         </div>

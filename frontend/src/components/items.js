5b0474ee5018d5832643497e64364f61aa6284b2
Volodya Sargsyan <volsarg@gmail.com>
2023-07-20 17:17:28 +0400

    create items

--- frontend/src/components/items.js
+++ frontend/src/components/items.js
@@ -1,8 +1,12 @@
 import React from "react";
+import '../style/Items.scss'
 
 export default function Items() {
     return (
-        <div>
+        <div className="items_container">
+            <p>mi</p>
+            <h1>250</h1>
+            <p>nash dvor</p>
         </div>
     )
 }
--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,10 +1,20 @@
 import React from "react";
+import Items from "../components/items";
 
 export default function Home() {
     return (
         <div>
             <div></div>
-            <div></div>
+            <div>
+                <div>
+                    <Items />
+                    <Items />
+                </div>
+                <div>
+                    <Items />
+                    <Items />
+                </div>
+            </div>
         </div>
     )
 }

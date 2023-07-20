2fbfba6f2d8f1daf6417d98b1c8d40a78cca8944
Volodya Sargsyan <volsarg@gmail.com>
2023-07-20 13:04:33 +0400

    router change

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,13 @@
 import React from "react";
-// import { Routes, Route } from "react-router";
+import { Routes, Route } from "react-router";
 import { store } from "./store";
 import { Provider } from "react-redux";
-// import Home from "./pages/Home"
-// import Technology from "./pages/Technology";
-// import FlightSchedle from "./pages/FlightSchedule";
-// import Garaiti from "./pages/Garaiti";
-// import AboutCompany from "./pages/AboutCompany";
-// import Contacts from "./pages/Contacts";
+import Home from "./pages/Home"
+import Technology from "./pages/Technology";
+import FlightSchedle from "./pages/FlightSchedule";
+import Garaiti from "./pages/Garaiti";
+import AboutCompany from "./pages/AboutCompany";
+import Contacts from "./pages/Contacts";
 import "./style/app.scss"
 import Header from "./components/Header";
 import RootRouter from "./router/routes";
@@ -17,7 +17,15 @@ export default function App() {
         <Provider store={store}>
             <div className="app">
                 <Header />
-                <RootRouter />
+                <Routes>            
+                    <Route path="*" element={<Error />} />
+                    <Route path="/" element={<Home />} />
+                    <Route path="/technology" element={<Technology />} />
+                    <Route path="/flightschedle" element={<FlightSchedle />} />
+                    <Route path="/garaiti" element={<Garaiti />} />
+                    <Route path="/aboutcompany" element={<AboutCompany />} />
+                    <Route path="/contacts" element={<Contacts />} />
+                </Routes>
             </div>
         </Provider>
     )
--- frontend/src/router/routes.js
+++ frontend/src/router/routes.js
@@ -1,8 +1,5 @@
 import React from "react";
-import {
-    createBrowserRouter,
-    RouterProvider,
-  } from "react-router-dom";
+import {createBrowserRouter, createRoutesFromElements, Route, Routes, RouterProvider} from "react-router-dom";
 import Error from "../pages/Error";
 import Home from "../pages/Home";
 import Technology from "../pages/Technology";
@@ -12,35 +9,18 @@ import AboutCompany from "../pages/AboutCompany";
 import Contacts from "../pages/Contacts";
 
 export default function RootRouter() {
-    const routes = createBrowserRouter([
-        {
-            path: "*",
-            element: <Error />,
-        },
-        {
-            path: "/",
-            element: <Home />,
-        },
-        {
-            path: "/technology",
-            element: <Technology />,
-        },
-        {
-            path: "/flightschedle",
-            element: <FlightSchedle />,
-        },
-        {
-            path: "/garaiti",
-            element: <Garaiti />,
-        },
-        {
-            path: "/aboutcompany",
-            element: <AboutCompany />,
-        },
-        {
-            path: "/contacts",
-            element: <Contacts />,
-        },
-    ])
+    const routes = createBrowserRouter(
+        createRoutesFromElements(
+            <Routes>            
+                <Route path="*" element={<Error />} />
+                <Route path="/" element={<Home />} />
+                <Route path="/technology" element={<Technology />} />
+                <Route path="/flightschedle" element={<FlightSchedle />} />
+                <Route path="/garaiti" element={<Garaiti />} />
+                <Route path="/aboutcompany" element={<AboutCompany />} />
+                <Route path="/contacts" element={<Contacts />} />
+            </Routes>
+        )
+    )
     return <RouterProvider router={routes} />;
 }
